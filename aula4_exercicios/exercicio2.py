9e4e2980ab3ce6ae1989ae6716b49fd605067ea4
Ivan Paulino <softip@gmail.com>
2024-03-12 13:44:51 -0300

    Conclusão dos exercícios

--- aula4_exercicios/exercicio2.py
+++ aula4_exercicios/exercicio2.py
@@ -12,3 +12,28 @@ Exemplo de Entrada
 Exemplo Saída
 O resultado da soma é 3.5
 '''
+
+#Entradas
+n1 = float(input("Digite um número: "))
+n2 = float(input("Digite um número: "))
+op = input("Digite operador matemático: ")
+
+#processamento
+if op == '+':
+    resultado = n1 + n2
+    print('O resultado da soma é ', resultado)
+elif op == '-':
+    resultado = n1 - n2
+    print('O resultado da subtração é ', resultado)
+elif op == '/':
+    resultado = n1 / n2
+    print('O resultado da divisão é ', resultado)
+elif op == '*':
+    resultado = n1 * n2
+    print('O resultado da multiplicação é ', resultado)
+else:
+    print('Operador desconhecido')
+
+
+
+
--- aula4_exercicios/exercicio3.py
+++ aula4_exercicios/exercicio3.py
@@ -7,3 +7,31 @@ perguntar se o usuário deseja continuar a calcular, se a resposta
 for S (Sim) o programa deve continuar se a resposta for N (Não) 
 o programa deve terminar.
 '''
+
+
+while True:
+    desejo = input("Deseja continuar? [S/N]")
+    if desejo == "N":
+        break
+    else:
+        #Entradas
+        n1 = float(input("Digite um número: "))
+        n2 = float(input("Digite um número: "))
+        op = input("Digite operador matemático: ")
+
+        #processamento
+        if op == '+':
+            resultado = n1 + n2
+            print('O resultado da soma é ', resultado)
+        elif op == '-':
+            resultado = n1 - n2
+            print('O resultado da subtração é ', resultado)
+        elif op == '/':
+            resultado = n1 / n2
+            print('O resultado da divisão é ', resultado)
+        elif op == '*':
+            resultado = n1 * n2
+            print('O resultado da multiplicação é ', resultado)
+        else:
+            print('Operador desconhecido')
+
